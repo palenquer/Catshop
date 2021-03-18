2ef85ba1917fb87c55a3bdd89928c3afc47d0e3b
Paulo Alenquer <pauloalenquer@gmail.com>
2021-03-18 20:53:29 -0300

    fixed grid

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -5,7 +5,7 @@ export default function PostList({ children }) {
 
 
   return (
-    <section className="w-full h-full grid grid-flow-col md:grid-flow-row md:grid-cols-2 grid-rows-4 md:grid-rows-3 gap-4 mt-8 mb-8">
+    <section className=" w-full h-full grid grid-flow-col md:grid-flow-row md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 grid-rows-4 md:grid-rows-3 gap-4 mt-8 mb-8">
       {children}
     </section>
   );
